e0f222ce2c580b4a585aad2487e34ee5b800075a
eheard00 <evanheard2000@gmail.com>
2025-01-22 22:53:30 -0600

    Update asteroid random move

--- asteroid.py
+++ asteroid.py
@@ -19,11 +19,14 @@ class Asteroid(CircleShape):
 			return
 		else:
 			angle = random.uniform(20,50)
-			vector1 = pygame.math.Vector2.rotate(self.velocity, angle)
-			vector2 = pygame.math.Vector2.rotate(self.velocity, -angle)
-			radius1 = self.radius - ASTEROID_MIN_RADIUS
-			radius2 = self.radius - ASTEROID_MIN_RADIUS
-			asteroid1 = Asteroid(self.position.x, self.position.y, radius1)
+
+			vector1 = self.velocity.rotate(angle)
+			vector2 = self.velocity.rotate(-angle)
+
+			new_radius = self.radius - ASTEROID_MIN_RADIUS
+			
+			asteroid1 = Asteroid(self.position.x, self.position.y, new_radius)
 			asteroid1.velocity = vector1 * 1.2
-			asteroid2 = Asteroid(self.position.x, self.position.y, radius2)
+
+			asteroid2 = Asteroid(self.position.x, self.position.y, new_radius)
 			asteroid2.velocity = vector2 * 1.2
